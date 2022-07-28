7abf92b04e4a774b059f6ab1e33bca292cfafada
OwaisQureshi2526 <91030756+OwaisQureshi2526@users.noreply.github.com>
2022-07-29 04:38:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function onbulb(){
+    document.getElementById("bulb").src = "./onbulb.jpg"
+}
+
+function offbulb(){
+    document.getElementById("bulb").src = "./offbulb.jpg"
+}
+function yellow(){
+    document.getElementById("traffic").src = "./yellow.png"
+}
+function green(){
+    document.getElementById("traffic").src = "./green.png"
+}
